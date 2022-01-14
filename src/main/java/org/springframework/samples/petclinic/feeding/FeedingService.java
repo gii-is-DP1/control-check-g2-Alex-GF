59804410277f90320a41b6eaaa6021f4363ad26d
Alex-GF <alegarfer4@alum.us.es>
2022-01-14 09:40:13 +0100

    Entrega alegarfer4

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,21 +2,38 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class FeedingService {
+
+    private FeedingRepository feedingRepository;
+
+    @Autowired
+    public FeedingService(FeedingRepository feedingRepository) {
+        this.feedingRepository = feedingRepository;
+    }
+
     public List<Feeding> getAll(){
-        return null;
+        return feedingRepository.findAll();
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+        return feedingRepository.findAllFeedingTypes();
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        return feedingRepository.findFeedingTypeByName(typeName);
     }
 
+    @Transactional(rollbackFor = UnfeasibleFeedingException.class)
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+        if(p.getPet().getType() != p.getFeedingType().getPetType()){
+            throw new UnfeasibleFeedingException();
+        }
+        return feedingRepository.save(p);       
     }
 
     
--- src/main/java/org/springframework/samples/petclinic/feeding/UnfeasibleFeedingException.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/UnfeasibleFeedingException.java
@@ -1,5 +1,4 @@
 package org.springframework.samples.petclinic.feeding;
 
 public class UnfeasibleFeedingException extends Exception {
-    
 }
